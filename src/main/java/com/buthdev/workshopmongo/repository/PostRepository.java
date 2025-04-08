4e25652611b3ebb57c7c6dbe3cd187d86c5b6745
Matheus <matheusdsouzabuth@gmail.com>
2025-04-08 10:39:39 -0300

    Creating Post entity with User

---
+++ src/main/java/com/buthdev/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.buthdev.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.buthdev.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
